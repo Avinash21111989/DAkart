bd962dd727462972f06942bab33c32bdf230760e
= <=>
2023-11-09 23:06:54 +0530

    Login here

--- DAkart/carts/views.py
+++ DAkart/carts/views.py
@@ -14,6 +14,7 @@ def _cart_id(request):
     
 
 def add_cart(request,product_id):
+    
     product = Product.objects.get(id=product_id)
     try:
         cart = Cart.objects.get(cart_id =_cart_id(request))
