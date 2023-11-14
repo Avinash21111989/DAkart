ccbaf8fa2d4e75350ed415829dbfba33955d987d
Papitha Tamilselvan <papitha06@gmail.com>
2023-11-14 15:14:07 -0500

    new changes

--- DAkart/carts/models.py
+++ DAkart/carts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-from store.models import Product, Variation
+from store.models import Product
 
 # Create your models here.
 class Cart(models.Model):
@@ -12,12 +11,11 @@ class Cart(models.Model):
 
 class CartItem(models.Model):
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
-    variation = models.ManyToManyField(Variation,blank=True)
     cart = models.ForeignKey(Cart,on_delete=models.CASCADE) 
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.product
     
 
