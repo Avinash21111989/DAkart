84fa9f95259ccb04afd0a93731975bf14b57b8be
kiruvino <147014300+kiruvino@users.noreply.github.com>
2023-11-22 18:09:21 -0600

    Revert "Kiruthiga vinoth"

--- carts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from carts.models import Cart
-from .models import CartItem
-
-# Register your models here.
-admin.site.register(Cart),
-admin.site.register(CartItem)
